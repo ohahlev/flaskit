ce69f32ffa1f417c35fd132e0348a52b2b6b601a
ah lev <ohahlev@gmail.com>
2019-09-29 18:54:02 +0700

    more update

--- app/views/frontend/site.py
+++ app/views/frontend/site.py
@@ -1,5 +1,6 @@
 from flask import (
-    Blueprint, render_template, jsonify, send_file, redirect, url_for
+    Blueprint, render_template, request,
+    redirect, url_for, session
 )
 
 bp = Blueprint("sitefe", __name__)
@@ -17,3 +18,17 @@ def about():
 def contact():
     return render_template("frontend/site/contact.html", title="Contact")
 
+@bp.route("/lang", methods=["POST"])
+def switch_lang():
+    print("switch lange")
+    lang = request.form.get("selected_lang")
+    try:
+        session_lang = session["lang"]
+        if lang != session_lang:
+            session["lang"] = lang
+    except KeyError:
+        session["lang"] = lang
+    current_page = request.form.get("current_page")
+    if not current_page:
+        return redirect(url_for("sitefe.index"))
+    return redirect(current_page)
